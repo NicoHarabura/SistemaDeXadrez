653fe761a062af7d786aa9aa8a3cae318d2c1e9a
NicoHarabura <nharabura@hotmail.com>
2019-04-30 16:43:17 -0300

    Cridas Classes: Tabuleiro e Peca

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,14 @@
 package application;
 
-import boardGame.Posicao;
+import boardgame.Tabuleiro;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		Posicao pos = new Posicao(5, 3);
-		System.out.println(pos);
-		pos.setValores(3, 5);
-		System.out.println(pos);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
+		
 	}
 
 }
