53a4e5077c7974fb3ba99a127296919fbacf0aa7
NicoHarabura <nharabura@hotmail.com>
2019-05-07 16:55:59 -0300

    Final

--- src/application/UI.java
+++ src/application/UI.java
@@ -122,7 +122,7 @@ public class UI {
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_RESET);
 		
-		System.out.print("White: ");
+		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
 		System.out.print(ANSI_RESET);
